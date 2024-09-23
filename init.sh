16fedcd80ac9c6b70119c3ba03b6543e337e7491
trancedj2022 <134460924+trancedj2022@users.noreply.github.com>
2024-09-23 10:27:43 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -108,13 +108,12 @@ EOF
   fi
 
   # 下载需要的应用
-  DASHBOARD_LATEST=$(wget -qO- "${GH_PROXY}https://api.github.com/repos/naiba/nezha/releases/latest" | awk -F '"' '/"tag_name"/{print $4}')
-  wget -O /tmp/dashboard.zip ${GH_PROXY}https://github.com/naiba/nezha/releases/download/$DASHBOARD_LATEST/dashboard-linux-$ARCH.zip
-  unzip -o /tmp/dashboard.zip -d /tmp
-  mv -f /tmp/dashboard-linux-$ARCH $WORK_DIR/app
+  wget -O /tmp/dashboard.zip ${GH_PROXY}https://github.com/naiba/nezha/releases/download/v0.18.11/dashboard-linux-$ARCH.zip
+  unzip /tmp/dashboard.zip -d /tmp
+  mv -f /tmp/dist/dashboard-linux-$ARCH $WORK_DIR/app
   wget -qO $WORK_DIR/cloudflared ${GH_PROXY}https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-$ARCH
-  wget -O $WORK_DIR/nezha-agent.zip ${GH_PROXY}https://github.com/nezhahq/agent/releases/latest/download/nezha-agent_linux_$ARCH.zip
-  unzip -o $WORK_DIR/nezha-agent.zip -d $WORK_DIR/
+  wget -O $WORK_DIR/nezha-agent.zip ${GH_PROXY}https://github.com/nezhahq/agent/releases/download/v0.17.5/nezha-agent_linux_$ARCH.zip
+  unzip $WORK_DIR/nezha-agent.zip -d $WORK_DIR/
   rm -rf $WORK_DIR/nezha-agent.zip /tmp/dist /tmp/dashboard.zip
 
   # 根据参数生成哪吒服务端配置文件
